fbbeed4971d3f3d236ce9c413f6ffa6763f378c5
goldtg <kangsun61@gmail.com>
2023-04-15 02:08:44 +0900

    feat: 설치 사례 추가

--- pages/refrigerator.vue
+++ pages/refrigerator.vue
@@ -107,5 +107,73 @@ export default {
         설치 사례
       </h1>
     </div>
+    <div class="grid grid-cols-3 w-1/2 mx-auto gap-4">
+      <div class="self-center w-[271px]">
+        <img
+          src="@/assets/refrigerator/Rectangle71.png"
+          alt="낙농 쇼케이스"
+        ><div class="text-center w-[271px] font-bold text-xl">
+          낙농 쇼케이스
+        </div>
+      </div>
+      <div class="self-center w-[271px]">
+        <img
+          src="@/assets/refrigerator/Rectangle72.png"
+          alt="마트장식장설치"
+        ><div class="text-center w-[271px] font-bold text-xl">
+          마트장식장설치
+        </div>
+      </div><div class="self-center w-[271px]">
+        <img
+          src="@/assets/refrigerator/Rectangle73.png"
+          alt="꽃 쇼케이스"
+        ><div class="text-center w-[271px] font-bold text-xl">
+          꽃 쇼케이스
+        </div>
+      </div><div class="self-center w-[271px]">
+        <img
+          src="@/assets/refrigerator/Rectangle74.png"
+          alt="낙농 쇼케이스"
+        ><div class="text-center w-[271px] font-bold text-xl">
+          낙농 쇼케이스
+        </div>
+      </div><div class="self-center w-[271px]">
+        <img
+          src="@/assets/refrigerator/Rectangle75.png"
+          alt="마트장식장설치"
+        ><div class="text-center w-[271px] font-bold text-xl">
+          마트장식장설치
+        </div>
+      </div><div class="self-center w-[271px]">
+        <img
+          src="@/assets/refrigerator/Rectangle76.png"
+          alt="꽃 쇼케이스"
+        ><div class="text-center w-[271px] font-bold text-xl">
+          꽃 쇼케이스
+        </div>
+      </div><div class="self-center w-[271px]">
+        <img
+          src="@/assets/refrigerator/Rectangle68.png"
+          alt="낙농 쇼케이스"
+        ><div class="text-center w-[271px] font-bold text-xl">
+          낙농 쇼케이스
+        </div>
+      </div><div class="self-center w-[271px]">
+        <img
+          src="@/assets/refrigerator/Rectangle69.png"
+          alt="마트장식장설치"
+        ><div class="text-center w-[271px] font-bold text-xl">
+          마트장식장설치
+        </div>
+      </div><div class="self-center w-[271px]">
+        <img
+          src="@/assets/refrigerator/Rectangle70.png"
+          alt="꽃 쇼케이스"
+        ><div class="text-center w-[271px] font-bold text-xl">
+          꽃
+          쇼케이스
+        </div>
+      </div>
+    </div>
   </main>
 </template>
