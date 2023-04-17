a26ab7af39d643947e89aa0c5ab3f19c433890fe
goldtg <kangsun61@gmail.com>
2023-04-17 22:41:36 +0900

    feat:부속품, 콘베어, 기록장치, 냉동기 페이지 추가 및 완료

---
+++ pages/conveyor.vue
@@ -0,0 +1,130 @@
+<script>
+export default {
+  name: 'ConveyorPage'
+}
+</script>
+
+<template>
+  <header>
+    <div class="relative overflow-hidden mb-20">
+      <img
+        src="@/assets/refrigerator/refrigerator.png"
+        alt="이미지"
+        class="w-full h-[449px]"
+      >
+      <div class="absolute top-0 left-0 right-1/2 bottom-0 bg-[#1B426B]" />
+      <div class="absolute bottom-0 left-0 right-1/2 top-0 px-10 py-20 ">
+        <div class="flex flex-col">
+          <div class="mx-auto h-[1px] item-center w-[400px] bg-white border mb-12" />
+          <h2 class="md:text-6xl font-bold mb-4 text-white text-center">
+            콘베어 방음설비
+          </h2>
+          <div class="h-[1px] mx-auto item-center w-[400px] bg-white border mt-12 mb-12" />
+          <p class="text-2xl text-white text-center">
+            냉동 / 냉장 저온저장고 무인관리 경비시스템
+          </p>
+          <p class="text-center text-white text-lg ">
+            최고의 품질과 최고의 기술로 고객을 먼저 생각하는<br>
+            21세기를 선도하는 냉동창고 전문업체 (주)은성냉동산업입니다.
+          </p>
+        </div>
+      </div>
+    </div>
+  </header>
+  <main>
+    <div class="w-1/2 mx-auto mb-20">
+      <h1 class="text-4xl mb-20 text-[#494949] font-bold inline-block border-b-4 border-gray-300 w-full">
+        콘베어
+      </h1>
+      <img
+        src="@/assets/conveyor/goods_08_01.png"
+        alt="콘베어"
+        class="mb-20 mx-auto"
+      >
+    </div>
+    <div class="w-1/2 mx-auto mb-20">
+      <h1 class="text-4xl mb-20 text-[#494949] font-bold inline-block border-b-4 border-gray-300 w-full">
+        방음설비
+      </h1>
+      <img
+        src="@/assets/conveyor/goods_08_02.png"
+        alt="콘베어"
+        class="mb-20 mx-auto"
+      >
+    </div>
+    <div class="w-1/2 mx-auto mb-20">
+      <h1 class="text-4xl text-[#494949] font-bold inline-block border-b-4 border-gray-300 w-full">
+        설치 사례
+      </h1>
+    </div>
+    <div class="grid grid-cols-3 mx-auto w-1/2 gap-4">
+      <div class="self-center w-[271px]">
+        <img
+          src="@/assets/refrigerator/Rectangle71.png"
+          alt="낙농 쇼케이스"
+        ><div class="text-center w-[271px] font-bold text-xl">
+          낙농 쇼케이스
+        </div>
+      </div>
+      <div class="self-center w-[271px]">
+        <img
+          src="@/assets/refrigerator/Rectangle72.png"
+          alt="마트장식장설치"
+        ><div class="text-center w-[271px] font-bold text-xl">
+          마트장식장설치
+        </div>
+      </div><div class="self-center w-[271px]">
+        <img
+          src="@/assets/refrigerator/Rectangle73.png"
+          alt="꽃 쇼케이스"
+        ><div class="text-center w-[271px] font-bold text-xl">
+          꽃 쇼케이스
+        </div>
+      </div><div class="self-center w-[271px]">
+        <img
+          src="@/assets/refrigerator/Rectangle74.png"
+          alt="낙농 쇼케이스"
+        ><div class="text-center w-[271px] font-bold text-xl">
+          낙농 쇼케이스
+        </div>
+      </div><div class="self-center w-[271px]">
+        <img
+          src="@/assets/refrigerator/Rectangle75.png"
+          alt="마트장식장설치"
+        ><div class="text-center w-[271px] font-bold text-xl">
+          마트장식장설치
+        </div>
+      </div><div class="self-center w-[271px]">
+        <img
+          src="@/assets/refrigerator/Rectangle76.png"
+          alt="꽃 쇼케이스"
+        ><div class="text-center w-[271px] font-bold text-xl">
+          꽃 쇼케이스
+        </div>
+      </div><div class="self-center w-[271px]">
+        <img
+          src="@/assets/refrigerator/Rectangle68.png"
+          alt="낙농 쇼케이스"
+        ><div class="text-center w-[271px] font-bold text-xl">
+          낙농 쇼케이스
+        </div>
+      </div><div class="self-center w-[271px]">
+        <img
+          src="@/assets/refrigerator/Rectangle69.png"
+          alt="마트장식장설치"
+        ><div class="text-center w-[271px] font-bold text-xl">
+          마트장식장설치
+        </div>
+      </div>
+      <div class="self-center w-[271px]">
+        <img
+          src="@/assets/refrigerator/Rectangle70.png"
+          alt="꽃 쇼케이스"
+        ><div class="text-center w-[271px] font-bold text-xl">
+          꽃
+          쇼케이스
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
