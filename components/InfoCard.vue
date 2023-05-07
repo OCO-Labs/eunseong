cbd34f9ef4af2d05981315bae5b91c4699c2d816
yemson <lier@kakao.com>
2023-05-07 13:01:14 +0900

    ...

--- components/InfoCard.vue
+++ components/InfoCard.vue
@@ -23,6 +23,16 @@ async function getRecentNotice () {
     })
   })
 }
+
+function formatDate (timestamp) {
+  const date = new Date(timestamp.seconds * 1000 + timestamp.nanoseconds / 1000000)
+
+  const year = date.getFullYear()
+  const month = String(date.getMonth() + 1).padStart(2, '0')
+  const day = String(date.getDate()).padStart(2, '0')
+
+  return `${year}-${month}-${day}`
+}
 </script>
 
 <template>
@@ -45,7 +55,7 @@ async function getRecentNotice () {
               {{ notice.title }}
             </div>
             <div class="text-[0.65rem] font-bold text-gray-400 place-self-center">
-              2022-01-10
+              {{ formatDate(notice.createdAt) }}
             </div>
           </div>
         </div>
